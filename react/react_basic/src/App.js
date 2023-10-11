a6bdc7f4f922b9b3927a8890b35be76aa3a0714c
DeepPoradiya <deepporadiaya@gmail.com>
2023-10-11 12:14:49 +0530

    in texform.js page create removes extra sapce method for the text and create a toggle switch button for dark mode

--- react/react_basic/src/App.js
+++ react/react_basic/src/App.js
@@ -1,15 +1,31 @@
+import { useState } from "react";
 import "./App.css";
-import About from "./componets/About";
+// import About from "./componets/About";
 import Navbar from "./componets/Navbar";
-// import Textforms from "./componets/Textforms";
+import Textforms from "./componets/Textforms";
 
 function App() {
+
+  const [mode,setmode]=useState('light');
+
+  const toggleMode=()=>{
+
+    if(mode==='light')
+    {
+      setmode('dark');
+    }
+    else{
+      setmode('light');
+
+    }
+  }
+
   return (
     <>
-      {/* <Navbar title="TextUtiles" abouttext="About Us" /> */}
-      <Navbar />
-      {/* <Textforms title="Counter" /> */}
-      <About />
+      <Navbar title="TextUtiles" abouttext="About Us" mode={mode} toggleMode={toggleMode}/>
+      {/* <Navbar  /> */}
+      <Textforms title="Counter" mode={mode} toggleMode={toggleMode} />
+      {/* <About /> */}
     </>
   );
 }
