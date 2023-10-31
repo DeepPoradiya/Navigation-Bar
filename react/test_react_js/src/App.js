002d564840dffbda0cca4ebd5b7a8535dceb2f47
DeepPoradiya <deepporadiaya@gmail.com>
2023-10-31 17:09:12 +0530

    fetch the data from API and create a student information table whenever user input the value fetch and this value print another table

--- react/test_react_js/src/App.js
+++ react/test_react_js/src/App.js
@@ -1,16 +1,23 @@
 import React from "react";
-import { BrowserRouter as Main, Route, Routes } from "react-router-dom";
-import Post from "./components/Post";
+// import { BrowserRouter as Main, Route, Routes } from "react-router-dom";
+// import Post from "./components/Post";
+import Form from "./components/Form";
 
 function App() {
-  return (
-    <Main>
-      <Routes>
-        <Route path="/" element={<Post />} />
-        <Route exact path="/:id" element={<Post />} />
-      </Routes>
-    </Main>
-  );
+  // return (
+  //   <Main>
+  //     <Routes>
+  //       <Route path="/" element={<Post />} />
+  //       <Route exact path="/:id" element={<Post  />} />
+  //     </Routes>
+  //   </Main>
+  // );
+  return <>
+    <div >
+      <h1 className="text-center">STUDENT INFORMATION</h1>
+      <Form/>
+    </div>
+  </>;
 }
 
 export default App;
--- react/test_react_js/src/components/Postitem.js
+++ react/test_react_js/src/components/Postitem.js
@@ -4,29 +4,29 @@ function Postitem(props) {
 
   return (
     <div>
-      <div className="card ">
+      <div className="card bg-dark-subtle border border-0 shadow">
         <div className="card-body">
-          <h5 className="card-title">{props.id}. {props.name}</h5>
+          <h5 className="card-title fw-bold border-bottom border-1 border-black  ">{props.id}.{props.name}</h5>
           <div className="card-text">
             <div className="row ">
-                <div className="col-5">Username</div>
-                <div className="col">{props.username}</div>
+                <div className="col-5 fw-bold">Username :</div>
+                <div className="col ">{props.username}</div>
             </div>
             <div className="row">
-                <div className="col-5">Email</div>
-                <div className="col text-break">{props.email}</div>
+                <div className="col-5 fw-bold">Email :</div>
+                <div className="col text-break ">{props.email}</div>
             </div>
             <div className="row">
-                <div className="col-5">Address</div>
-                <div className="col ">{props.address}</div>
+                <div className="col-5 fw-bold">Address :</div>
+                <div className="col  ">{props.address}</div>
             </div>
             <div className="row">
-                <div className="col-5">Phone</div>
-                <div className="col">{props.phone}</div>
+                <div className="col-5 fw-bold">Phone :</div>
+                <div className="col ">{props.phone}</div>
             </div>
             <div className="row">
-                <div className="col-5">Website</div>
-                <div className="col">{props.website}</div>
+                <div className="col-5 fw-bold">Website :</div>
+                <div className="col ">{props.website}</div>
             </div>
           </div>
         
