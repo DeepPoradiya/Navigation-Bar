fd10b8ea696c03c41aa0de56288e0d65f19d4935
DeepPoradiya <deepporadiaya@gmail.com>
2023-11-03 18:42:31 +0530

    Fetch API Task complate 100%  using useEffect,useState,UseLocation,useParam,Link and create a  function for show all data.

--- react/test_react_js/src/App.js
+++ react/test_react_js/src/App.js
@@ -1,14 +1,15 @@
 import React from "react";
 import { BrowserRouter as Main, Route, Routes } from "react-router-dom";
-import Post from "./components/Post";
+import Allpages from "./components/Allpages";
+import Page from "./components/Page"
 // import Form from "./components/Form";
 
 function App() {
   return (
     <Main>
       <Routes>
-        <Route path="/" element={<Post />} />
-        <Route exact path="/page/:id" element={<Post  />} />
+        <Route path="/" element={<Allpages />} />
+        <Route exact path="/Page/:id" element={<Page  />} />
       </Routes>
     </Main>
   );
